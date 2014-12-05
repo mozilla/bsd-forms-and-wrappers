eb8a702da3c81e89ca576596b62e0ff99510deae
Ali Al Dallal <ali@alicoding.com>
2014-12-05 08:54:36 -0800

    bug1106929 - Add simple paypal form

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,209 +1,136 @@
-var generate_bake_content = function(locale, paypal_locale, show_email_signup, currency, currency_display, return_url) {
-  var bake_content = require("./0-donation-forms/locales/" + locale + "/bsd-form-donation.json");
-
-  bake_content["currency"] = currency;
-  bake_content["currency_display"] = currency_display;
-  bake_content["locale"] = paypal_locale;
-  bake_content["return_url"] = return_url;
-  bake_content["show_email_signup"] = show_email_signup;
-
-  return bake_content;
-};
-
-var generatePayPalPage = function(currency_code, min_amount, currency_symbol, paypal_locale, locale, currency_dup) {
-  var path_code = currency_dup ? currency_dup : currency_code;
-  var locale_content;
-  if(locale) {
-    locale_content = require("./0-content-page/EOYFR2014-PayPal-Donate/locales/" + currency_code + "/locale-" + locale + ".json");
-  } else {
-    locale_content = require("./0-content-page/EOYFR2014-PayPal-Donate/locales/USD/locale.json");
-  }
-  locale_content["currency_code"] = currency_code;
-  locale_content["min_amount"] = min_amount;
-  locale_content["currency_symbol"] = currency_symbol;
-  locale_content["locale"] = paypal_locale;
-
-  locale_content = JSON.parse(JSON.stringify(locale_content).replace(/{{ min_amount }}/, function(m, k) {
-    return min_amount
-  }));
-  locale_content = JSON.parse(JSON.stringify(locale_content).replace(/{{ currency_symbol }}/, function(m, k) {
-    return currency_symbol
-  }));
-
-  var bake_content = {
-    options: {
-      content: locale_content
-    },
-    files: {}
-  };
-  bake_content.files["compiled/paypal/" + path_code + "/EOYFR2014-PayPal-Donate.html"] = "0-content-page/EOYFR2014-PayPal-Donate/template.html";
-  bake_content.files["compiled/paypal/" + path_code + "/EOYFR2014-PayPal-Donate-content.html"] = "0-content-page/EOYFR2014-PayPal-Donate/page-content.html";
-  return bake_content;
-};
-
 module.exports = function(grunt) {
+
+  var generate_bake_files_content = function(locale, paypal_locale, show_email_signup, currency, currency_display, return_url, arr_amounts, currency_symbol) {
+    var obj = {};
+    var bake_content = require("./0-donation-forms/locales/" + locale + "/bsd-form-donation.json");
+    bake_content = grunt.util._.merge(bake_content, require('./0-content-page/EOYFR2014-Button-Donation/locales/USD/locale.json'));
+
+    bake_content["currency"] = currency;
+    bake_content["currency_symbol"] = currency;
+    bake_content["currency_display"] = currency_display;
+    bake_content["locale"] = paypal_locale;
+    bake_content["return_url"] = return_url;
+    bake_content["show_email_signup"] = show_email_signup;
+    bake_content = JSON.parse(JSON.stringify(bake_content).replace(/{{ currency_symbol }}/, function(m, k) {
+      return currency_symbol
+    }));
+    if(arr_amounts) {
+      bake_content["amount_idx_1"] = arr_amounts[0];
+      bake_content["amount_idx_2"] = arr_amounts[1];
+      bake_content["amount_idx_3"] = arr_amounts[2];
+      bake_content["amount_idx_4"] = arr_amounts[3];
+    }
+    obj = {
+      options: {
+        content: bake_content
+      },
+      files: {}
+    };
+    obj.files['compiled/' + locale + '/EOY-content-page-bitcoin-donate.html'] = '0-content-page/EOYFR2014-Bitcoin-Donate/template.html';
+    obj.files['compiled/' + locale + '/EOY-donation-form-single.html'] = '0-donation-forms/EOY-donation-form-single/template.html';
+    obj.files['compiled/' + locale + '/EOY-donation-form-single-above.html'] = '0-donation-forms/EOY-donation-form-single/above-form-content.html';
+    obj.files['compiled/' + locale + '/EOY-donation-form-single-below.html'] = '0-donation-forms/EOY-donation-form-single/below-form-content.html';
+    // sequential
+    obj.files['compiled/' + locale + '/EOY-donation-form-sequential.html'] = '0-donation-forms/EOY-donation-form-sequential/template.html';
+    obj.files['compiled/' + locale + '/EOY-donation-form-sequential-above.html'] = '0-donation-forms/EOY-donation-form-sequential/above-form-content.html';
+    obj.files['compiled/' + locale + '/EOY-donation-form-sequential-below.html'] = '0-donation-forms/EOY-donation-form-sequential/below-form-content.html';
+    // post donation
+    obj.files['compiled/' + locale + '/EOY-signup-form-post-donation.html'] = '0-email-signup-form/EOYFR2014-Donor/template.html';
+    obj.files['compiled/' + locale + '/EOY-signup-form-post-donation-above.html'] = '0-email-signup-form/EOYFR2014-Donor/above-form-content.html';
+    obj.files['compiled/' + locale + '/EOY-signup-form-post-donation-below.html'] = '0-email-signup-form/EOYFR2014-Donor/below-form-content.html';
+    // buttons
+    obj.files['compiled/' + locale + '/simple-paypal/EOY-simple-paypal.html'] = '0-content-page/EOYFR2014-Button-Donation/template.html';
+    obj.files['compiled/' + locale + '/simple-paypal/EOY-simple-paypal-content.html'] = '0-content-page/EOYFR2014-Button-Donation/page-content.html';
+
+    return obj;
+  };
+
+  var generatePayPalPage = function(currency_code, min_amount, currency_symbol, paypal_locale, locale, currency_dup) {
+    var path_code = currency_dup ? currency_dup : currency_code;
+    var locale_content;
+    if(locale) {
+      locale_content = require("./0-content-page/EOYFR2014-PayPal-Donate/locales/" + currency_code + "/locale-" + locale + ".json");
+    } else {
+      locale_content = require("./0-content-page/EOYFR2014-PayPal-Donate/locales/USD/locale.json");
+    }
+    locale_content["currency_code"] = currency_code;
+    locale_content["min_amount"] = min_amount;
+    locale_content["currency_symbol"] = currency_symbol;
+    locale_content["locale"] = paypal_locale;
+
+    locale_content = JSON.parse(JSON.stringify(locale_content).replace(/{{ min_amount }}/, function(m, k) {
+      return min_amount
+    }));
+    locale_content = JSON.parse(JSON.stringify(locale_content).replace(/{{ currency_symbol }}/, function(m, k) {
+      return currency_symbol
+    }));
+
+    var bake_content = {
+      options: {
+        content: locale_content
+      },
+      files: {}
+    };
+    bake_content.files["compiled/paypal/" + path_code + "/EOYFR2014-PayPal-Donate.html"] = "0-content-page/EOYFR2014-PayPal-Donate/template.html";
+    bake_content.files["compiled/paypal/" + path_code + "/EOYFR2014-PayPal-Donate-content.html"] = "0-content-page/EOYFR2014-PayPal-Donate/page-content.html";
+    return bake_content;
+  };
+
   require('time-grunt')(grunt);
   require('jit-grunt')(grunt, {});
 
   grunt.initConfig({
     bake: {
-      'en_US': {
-        options: {
-          content: generate_bake_content(
-            "en_US",
-            "US",
-            true,
-            "USD",
-            "",
-            "https://sendto.mozilla.org/page/s/EOYFR2014-donor"
-          )
-        },
-        files: {
-          // add whichever files you're working on here
-          'compiled/en_US/EOY-content-page-bitcoin-donate.html': '0-content-page/EOYFR2014-Bitcoin-Donate/template.html',
-          'compiled/en_US/EOY-donation-form-single.html': '0-donation-forms/EOY-donation-form-single/template.html',
-          'compiled/en_US/EOY-donation-form-single-above.html': '0-donation-forms/EOY-donation-form-single/above-form-content.html',
-          'compiled/en_US/EOY-donation-form-single-below.html': '0-donation-forms/EOY-donation-form-single/below-form-content.html',
-          // sequential
-          'compiled/en_US/EOY-donation-form-sequential.html': '0-donation-forms/EOY-donation-form-sequential/template.html',
-          'compiled/en_US/EOY-donation-form-sequential-above.html': '0-donation-forms/EOY-donation-form-sequential/above-form-content.html',
-          'compiled/en_US/EOY-donation-form-sequential-below.html': '0-donation-forms/EOY-donation-form-sequential/below-form-content.html',
-          // post donation
-          'compiled/en_US/EOY-signup-form-post-donation.html': '0-email-signup-form/EOYFR2014-Donor/template.html',
-          'compiled/en_US/EOY-signup-form-post-donation-above.html': '0-email-signup-form/EOYFR2014-Donor/above-form-content.html',
-          'compiled/en_US/EOY-signup-form-post-donation-below.html': '0-email-signup-form/EOYFR2014-Donor/below-form-content.html'
-        }
-      },
-      'de': {
-        options: {
-          content: generate_bake_content(
-            "de",
-            "DE",
-            false,
-            "USD",
-            "USD",
-            "https://sendto.mozilla.org/page/s/EOYFR2014-donor-de"
-          )
-        },
-        files: {
-          // add whichever files you're working on here
-          'compiled/de/EOY-donation-form-single.html': '0-donation-forms/EOY-donation-form-single/template.html',
-          'compiled/de/EOY-donation-form-single-above.html': '0-donation-forms/EOY-donation-form-single/above-form-content.html',
-          'compiled/de/EOY-donation-form-single-below.html': '0-donation-forms/EOY-donation-form-single/below-form-content.html',
-          // sequential
-          'compiled/de/EOY-donation-form-sequential.html': '0-donation-forms/EOY-donation-form-sequential/template.html',
-          'compiled/de/EOY-donation-form-sequential-above.html': '0-donation-forms/EOY-donation-form-sequential/above-form-content.html',
-          'compiled/de/EOY-donation-form-sequential-below.html': '0-donation-forms/EOY-donation-form-sequential/below-form-content.html',
-          // post donation
-          'compiled/de/EOY-signup-form-post-donation.html': '0-email-signup-form/EOYFR2014-Donor/template.html',
-          'compiled/de/EOY-signup-form-post-donation-above.html': '0-email-signup-form/EOYFR2014-Donor/above-form-content.html',
-          'compiled/de/EOY-signup-form-post-donation-below.html': '0-email-signup-form/EOYFR2014-Donor/below-form-content.html'
-        }
-      },
-      'fr': {
-        options: {
-          content: generate_bake_content(
-            "fr",
-            "FR",
-            false,
-            "USD",
-            "USD",
-            "https://sendto.mozilla.org/page/s/EOYFR2014-donor-fr"
-          )
-        },
-        files: {
-          // add whichever files you're working on here
-          'compiled/fr/EOY-donation-form-single.html': '0-donation-forms/EOY-donation-form-single/template.html',
-          'compiled/fr/EOY-donation-form-single-above.html': '0-donation-forms/EOY-donation-form-single/above-form-content.html',
-          'compiled/fr/EOY-donation-form-single-below.html': '0-donation-forms/EOY-donation-form-single/below-form-content.html',
-          // sequential
-          'compiled/fr/EOY-donation-form-sequential.html': '0-donation-forms/EOY-donation-form-sequential/template.html',
-          'compiled/fr/EOY-donation-form-sequential-above.html': '0-donation-forms/EOY-donation-form-sequential/above-form-content.html',
-          'compiled/fr/EOY-donation-form-sequential-below.html': '0-donation-forms/EOY-donation-form-sequential/below-form-content.html',
-          // post donation
-          'compiled/fr/EOY-signup-form-post-donation.html': '0-email-signup-form/EOYFR2014-Donor/template.html',
-          'compiled/fr/EOY-signup-form-post-donation-above.html': '0-email-signup-form/EOYFR2014-Donor/above-form-content.html',
-          'compiled/fr/EOY-signup-form-post-donation-below.html': '0-email-signup-form/EOYFR2014-Donor/below-form-content.html'
-        }
-      },
-      'pt_BR': {
-        options: {
-          content: generate_bake_content(
-            "pt_BR",
-            "pt_BR",
-            false,
-            "USD",
-            "USD",
-            "https://sendto.mozilla.org/page/s/EOYFR2014-donor-pt-BR"
-          )
-        },
-        files: {
-          // add whichever files you're working on here
-          'compiled/pt_BR/EOY-donation-form-single.html': '0-donation-forms/EOY-donation-form-single/template.html',
-          'compiled/pt_BR/EOY-donation-form-single-above.html': '0-donation-forms/EOY-donation-form-single/above-form-content.html',
-          'compiled/pt_BR/EOY-donation-form-single-below.html': '0-donation-forms/EOY-donation-form-single/below-form-content.html',
-          // sequential
-          'compiled/pt_BR/EOY-donation-form-sequential.html': '0-donation-forms/EOY-donation-form-sequential/template.html',
-          'compiled/pt_BR/EOY-donation-form-sequential-above.html': '0-donation-forms/EOY-donation-form-sequential/above-form-content.html',
-          'compiled/pt_BR/EOY-donation-form-sequential-below.html': '0-donation-forms/EOY-donation-form-sequential/below-form-content.html',
-          // post donation
-          'compiled/pt_BR/EOY-signup-form-post-donation.html': '0-email-signup-form/EOYFR2014-Donor/template.html',
-          'compiled/pt_BR/EOY-signup-form-post-donation-above.html': '0-email-signup-form/EOYFR2014-Donor/above-form-content.html',
-          'compiled/pt_BR/EOY-signup-form-post-donation-below.html': '0-email-signup-form/EOYFR2014-Donor/below-form-content.html'
-        }
-      },
-      'id': {
-        options: {
-          content: generate_bake_content(
-            "id",
-            "id_ID",
-            false,
-            "USD",
-            "USD",
-            "https://sendto.mozilla.org/page/s/EOYFR2014-donor-id"
-          )
-        },
-        files: {
-          // add whichever files you're working on here
-          'compiled/id/EOY-donation-form-single.html': '0-donation-forms/EOY-donation-form-single/template.html',
-          'compiled/id/EOY-donation-form-single-above.html': '0-donation-forms/EOY-donation-form-single/above-form-content.html',
-          'compiled/id/EOY-donation-form-single-below.html': '0-donation-forms/EOY-donation-form-single/below-form-content.html',
-          // sequential
-          'compiled/id/EOY-donation-form-sequential.html': '0-donation-forms/EOY-donation-form-sequential/template.html',
-          'compiled/id/EOY-donation-form-sequential-above.html': '0-donation-forms/EOY-donation-form-sequential/above-form-content.html',
-          'compiled/id/EOY-donation-form-sequential-below.html': '0-donation-forms/EOY-donation-form-sequential/below-form-content.html',
-          // post donation
-          'compiled/id/EOY-signup-form-post-donation.html': '0-email-signup-form/EOYFR2014-Donor/template.html',
-          'compiled/id/EOY-signup-form-post-donation-above.html': '0-email-signup-form/EOYFR2014-Donor/above-form-content.html',
-          'compiled/id/EOY-signup-form-post-donation-below.html': '0-email-signup-form/EOYFR2014-Donor/below-form-content.html'
-        }
-      },
-      'es': {
-        options: {
-          content: generate_bake_content(
-            "es",
-            "ES",
-            false,
-            "USD",
-            "USD",
-            "https://sendto.mozilla.org/page/s/EOYFR2014-donor-es"
-          )
-        },
-        files: {
-          // single
-          'compiled/es/EOY-donation-form-single.html': '0-donation-forms/EOY-donation-form-single/template.html',
-          'compiled/es/EOY-donation-form-single-above.html': '0-donation-forms/EOY-donation-form-single/above-form-content.html',
-          'compiled/es/EOY-donation-form-single-below.html': '0-donation-forms/EOY-donation-form-single/below-form-content.html',
-          // sequential
-          'compiled/es/EOY-donation-form-sequential.html': '0-donation-forms/EOY-donation-form-sequential/template.html',
-          'compiled/es/EOY-donation-form-sequential-above.html': '0-donation-forms/EOY-donation-form-sequential/above-form-content.html',
-          'compiled/es/EOY-donation-form-sequential-below.html': '0-donation-forms/EOY-donation-form-sequential/below-form-content.html',
-          // post donation
-          'compiled/es/EOY-signup-form-post-donation.html': '0-email-signup-form/EOYFR2014-Donor/template.html',
-          'compiled/es/EOY-signup-form-post-donation-above.html': '0-email-signup-form/EOYFR2014-Donor/above-form-content.html',
-          'compiled/es/EOY-signup-form-post-donation-below.html': '0-email-signup-form/EOYFR2014-Donor/below-form-content.html'
-        }
-      },
+      'en_US': generate_bake_files_content(
+        "en_US",
+        "US",
+        true,
+        "USD",
+        "",
+        "https://sendto.mozilla.org/page/s/EOYFR2014-donor",
+        [15, 10, 5, 3],
+        "$"
+      ),
+      'de': generate_bake_files_content(
+        "de",
+        "DE",
+        false,
+        "USD",
+        "USD",
+        "https://sendto.mozilla.org/page/s/EOYFR2014-donor-de"
+      ),
+      'fr': generate_bake_files_content(
+        "fr",
+        "FR",
+        false,
+        "USD",
+        "USD",
+        "https://sendto.mozilla.org/page/s/EOYFR2014-donor-fr"
+      ),
+      'pt_BR': generate_bake_files_content(
+        "pt_BR",
+        "pt_BR",
+        false,
+        "USD",
+        "USD",
+        "https://sendto.mozilla.org/page/s/EOYFR2014-donor-pt-BR"
+      ),
+      'id': generate_bake_files_content(
+        "id",
+        "id_ID",
+        false,
+        "USD",
+        "USD",
+        "https://sendto.mozilla.org/page/s/EOYFR2014-donor-id"
+      ),
+      'es': generate_bake_files_content(
+        "es",
+        "ES",
+        false,
+        "USD",
+        "USD",
+        "https://sendto.mozilla.org/page/s/EOYFR2014-donor-es"
+      ),
       // PayPal currencies
       'AUD': generatePayPalPage('AUD', 2, '$', 'US'),
       'BRL': generatePayPalPage('BRL', 5, 'R$', 'pt_BR', 'pt_BR'),
